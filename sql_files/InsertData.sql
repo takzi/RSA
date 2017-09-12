04b03396a09fdd615c15ea7a94d1479456b8981b
Anthony Perez <ajp8707@g.rit.edu>
2017-09-11 20:32:57 -0400

    Added "InsertData.sql" into sql_files folder.

---
+++ sql_files/InsertData.sql
@@ -0,0 +1,56 @@
+INSERT INTO `congregation` VALUES (0,??,'DUPC');
+INSERT INTO `congregation` VALUES (0,??,'First Presbyterian Pittsford');
+INSERT INTO `congregation` VALUES (0,??,"St. Paul's Episcopal");
+INSERT INTO `congregation` VALUES (0,??,'Two Saints');
+INSERT INTO `congregation` VALUES (0,??,'First Universalist');
+INSERT INTO `congregation` VALUES (0,??,'Incarnate World');
+INSERT INTO `congregation` VALUES (0,??,'Assumption');
+INSERT INTO `congregation` VALUES (0,??,'Asbury Methodist');
+INSERT INTO `congregation` VALUES (0,??,'Mary Magdalene');
+INSERT INTO `congregation` VALUES (0,??,'First Unitarian');
+INSERT INTO `congregation` VALUES (0,??,'Temple Sinai');
+INSERT INTO `congregation` VALUES (0,??,'Day Center');
+INSERT INTO `congregation` VALUES (0,??,'Third Presbyterian');
+
+
+
+INSERT INTO `congregation_supporting` VALUES (??,'Religious Society of Friends');
+
+INSERT INTO `congregation_supporting` VALUES (??,"St. Paul's Lutheran");
+INSERT INTO `congregation_supporting` VALUES (??,'Christ Church Pittsford');
+
+INSERT INTO `congregation_supporting` VALUES (??,'Incarnation');
+INSERT INTO `congregation_supporting` VALUES (??,'Good Shepherd');
+INSERT INTO `congregation_supporting` VALUES (??,'Webster UCC');
+INSERT INTO `congregation_supporting` VALUES (??,'Zen Center');
+
+INSERT INTO `congregation_supporting` VALUES (??,'Christ Episcopal');
+INSERT INTO `congregation_supporting` VALUES (??,'Elmgrove UCC');
+
+INSERT INTO `congregation_supporting` VALUES (??,'Aldersgate');
+INSERT INTO `congregation_supporting` VALUES (??,'Church of Love');
+INSERT INTO `congregation_supporting` VALUES (??,"St. Mary's");
+
+INSERT INTO `congregation_supporting` VALUES (??,'Messiah Lutheran');
+INSERT INTO `congregation_supporting` VALUES (??,'Bethany Presbyterian');
+
+INSERT INTO `congregation_supporting` VALUES (??,"St. Luke's");
+
+INSERT INTO `congregation_supporting` VALUES (??,'Baber AME');
+INSERT INTO `congregation_supporting` VALUES (??,'Baptist Temple');
+INSERT INTO `congregation_supporting` VALUES (??,'Greece Baptist');
+
+INSERT INTO `congregation_supporting` VALUES (??,'Transfiguration');
+INSERT INTO `congregation_supporting` VALUES (??,'St. Louis');
+INSERT INTO `congregation_supporting` VALUES (??,'St. Catherine');
+INSERT INTO `congregation_supporting` VALUES (??,'United Church of Pittsford');
+
+INSERT INTO `congregation_supporting` VALUES (??,'Temple Beth EL');
+
+INSERT INTO `congregation_supporting` VALUES (??,"St. Joseph's Penfield");
+
+INSERT INTO `congregation_supporting` VALUES (??,'ALL CONGREGATIONS');
+
+INSERT INTO `congregation_supporting` VALUES (??,'New Life Presbyterian');
+
+
