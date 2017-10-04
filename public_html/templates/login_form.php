f03527c4013dfb86ae10acacd901e5a85d0d5ba1
Kristen Merritt <km2029@rit.edu>
2017-10-04 12:38:26 -0400

    updatd login_form.php

--- public_html/templates/login_form.php
+++ public_html/templates/login_form.php
@@ -1,9 +1,11 @@
+<div class="form_header clearfix">
+		<h1>RSA Login</h1>
+	</div>
 <form id="login_form" method="POST">
-	<h1>RSA Login</h1>
 	<input type="text" placeholder="username" name="username" required>
 	<br><br>
 	<input type="password" placeholder="password" name="password" required>
 	<br><br>
 	<button type="submit">Login</button>
-	<button>Request Account</button>
+	<a id="request_href" href="request_account.php">Request Account</a>
 </form>
