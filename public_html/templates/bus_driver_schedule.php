a9640a5efc64ba5a523ede16e28871c84d7d218c
Chevy Mac <cjm4547@rit.edu>
2017-10-10 20:53:56 -0400

    Updated CSS and Bus Driver Schedule

--- public_html/templates/bus_driver_schedule.php
+++ public_html/templates/bus_driver_schedule.php
@@ -11,53 +11,228 @@
 	<table>
 		<tr>
 			<td id="cong">Cong 1</td>
-			<td>1</td>
-			<td>2</td>
-			<td>3</td>
-			<td>4</td>
-			<td>5</td>
-			<td>6</td>
-			<td>7</td>
+			<td>	1
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	2
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	3
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	4
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	5
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	6
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	7
+            M:
+            MB:
+            E:
+            EB:
+			</td>
 		</tr>
 		<tr>
 			<td id="cong">Cong 2</td>
-			<td>8</td>
-			<td>9</td>
-			<td>10</td>
-			<td>11</td>
-			<td>12</td>
-			<td>13</td>
-			<td>14</td>
+			<td>	8
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	9
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	10
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	12
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	12
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	13
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	14
+            M:
+            MB:
+            E:
+            EB:
+			</td>
 		</tr>
 		<tr>
 			<td id="cong">Cong 3</td>
-			<td>15</td>
-			<td>16</td>
-			<td>17</td>
-			<td>18</td>
-			<td>19</td>
-			<td>20</td>
-			<td>21</td>
+			<td>	15
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	16
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	17
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	18
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	19
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	20
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	21
+            M:
+            MB:
+            E:
+            EB:
+			</td>
 		</tr>
 		<tr>
 			<td id="cong">Cong 4</td>
-			<td>22</td>
-			<td>23</td>
-			<td>24</td>
-			<td>25</td>
-			<td>26</td>
-			<td>27</td>
-			<td>28</td>
+			<td>	22
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	23
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	24
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	25
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	26
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	27
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	28
+            M:
+            MB:
+            E:
+            EB:
+			</td>
 		</tr>
 		<tr>
 			<td id="cong">Cong 5</td>
-			<td>29</td>
-			<td>30</td>
-			<td>31</td>
-			<td>1</td>
-			<td>2</td>
-			<td>3</td>
-			<td>4</td>
+			<td>	29
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	30
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	31
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	1
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	2
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	3
+            M:
+            MB:
+            E:
+            EB:
+			</td>
+			<td>	4
+            M:
+            MB:
+            E:
+            EB:
+			</td>
 		</tr>
 	</table>
 </div>
