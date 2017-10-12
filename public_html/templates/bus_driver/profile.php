8f21b9867d88b9952d4558289fbc45c76c622e35
Chevy Mac <cjm4547@rit.edu>
2017-10-12 01:59:52 -0400

    Added Bus Driver Proifle and its CSS
    Going to fix it later on.

---
+++ public_html/templates/bus_driver/profile.php
@@ -0,0 +1,46 @@
+<?php
+	$page='RAHIN Home';
+	$path="./";
+	include("header.php");
+?>
+
+<div class="container">
+	<h1 id ="profile_h1">Congegation Name</h1>
+	<div id="profile_container">
+		<div align="left"> 
+			<button id ="profile_button">Action Required</button>
+			<button id ="profile_button">View/Request Blackout Dates</button>
+			<button id ="profile_button">Request Schedule Change</button>
+		</div>
+		<div align="right"> 
+			<table>
+				<tr>
+					<td colspan="3" id="title">Current Schedule</td>
+				<tr>
+				<tr>
+					<td>Jan 8</td>
+					<td>M</td>
+					<td>Congegation</td>
+				<tr>
+				<tr>
+					<td>Jan 9</td>
+					<td>E</td>
+					<td>Congegation</td>
+				<tr>
+				<tr>
+					<td>Jan 10</td>
+					<td>MB</td>
+					<td>Congegation</td>
+				<tr>
+				<tr>
+					<td>Jan 11</td>
+					<td>EB</td>
+					<td>Congegation</td>
+				<tr>
+			</table>
+		</div>
+	</div>
+</div> 
+<?php
+	include("footer.php");
+?>
