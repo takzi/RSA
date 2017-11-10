136ff014f8cc2013ce0c4383d1c1bf61a4ceda87
Chevy Mac <cjm4547@rit.edu>
2017-11-10 02:25:39 -0500

    Admin Generate Schedule, not sure how to add arrow button.

---
+++ public_html/templates/admin/admin_generate_schedule.php
@@ -0,0 +1,81 @@
+<?php
+	$page='RAHIN Admin Generate Schedule';
+	$path_to_root="./../../";
+
+	require_once($path_to_root.'../BUS/GeneralTemplate.class.php');
+	require_once($path_to_root.'../BUS/admin/AdminFunctions.class.php');
+
+	$generalTemplate = new GeneralTemplate($page, $path_to_root);
+	$adminFunctions = new AdminFunctions($page, $path_to_root);
+
+	echo $generalTemplate->insertHeader();
+	echo '<link href="'.$path_to_root.'css/admin.css" rel="stylesheet">';
+
+?>
+	<div id="admin_container">
+		<div id="generate">
+			<table id ="generate_schedule">
+				<tr>
+					<th colspan="2" id="title">Rotation: 50</th>
+				</tr>
+				<tr>
+					<td class="genDate">5/28/2017</td>
+					<td class="genSche">Congregation 1</td>
+				</tr>
+				<tr>
+					<td class="genDate">6/4/2017</td>
+					<td class="genSche">Congregation 1</td>
+				</tr>
+				<tr>
+					<td class="genDate">6/11/2017</td>
+					<td class="genSche">Congregation 1</td>
+				</tr>
+				<tr>
+					<td class="genDate">6/18/2017</td>
+					<td class="genSche">Congregation 1</td>
+				</tr>
+				<tr>
+					<td class="genDate">6/25/2017</td>
+					<td class="genSche">Congregation 1</td>
+				</tr>
+				<tr>
+					<td class="genDate">7/2/2017</td>
+					<td class="genSche">Congregation 1</td>
+				</tr>
+				<tr>
+					<td class="genDate">7/9/2017</td>
+					<td class="genSche">Congregation 1</td>
+				</tr>
+				<tr>
+					<td class="genDate">7/16/2017</td>
+					<td class="genSche">Congregation 1</td>
+				</tr>
+				<tr>
+					<td class="genDate">7/23/2017</td>
+					<td class="genSche">Congregation 1</td>
+				</tr>
+				<tr>
+					<td class="genDate">7/30/2017</td>
+					<td class="genSche">Congregation 1</td>
+				</tr>
+				<tr>
+					<td class="genDate">8/6/2017</td>
+					<td class="genSche">Congregation 1</td>
+				</tr>
+				<tr>
+					<td class="genDate">8/13/2017</td>
+					<td class="genSche">Congregation 1</td>
+				</tr>
+				<tr>
+					<td class="genDate">8/20/2017</td>
+					<td class="genSche">Congregation 1</td>
+				</tr>		
+			</table>
+			<div id="genBtnArea">
+				<input type='button' class ='genBtn' name='re-gen' value='Re-Generate'><input type='button' id='genRight' class ='genBtn' name='save-send' value='Save / Send'>
+			</div>
+		</div>	
+	</div>
+<?php
+	echo $generalTemplate->insertFooter();
+?>
