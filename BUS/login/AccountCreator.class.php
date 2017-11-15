e28e3693df460acde897d19b962c5e5fa4a01ce3
Kristen Merritt <km2029@rit.edu>
2017-11-15 17:46:48 -0500

    commented the AccountCreator.class.php file

--- BUS/login/AccountCreator.class.php
+++ BUS/login/AccountCreator.class.php
@@ -1,10 +1,27 @@
 <?php
+/**
+ * AccountCreator holds all of the functionality
+ * to create a new account in the RSA database.
+ *
+ *
+ * @author     Kristen Merritt
+ * @author     Tiandre Turner
+ * @author     Anthony Perez
+ * @version    Release: 1.0
+ * @date       11/15/2017
+ */
 
 class AccountCreator{
-	private $path_to_root;
-	private $db;
-	private $sanitizer;
+	private $path_to_root; // provide the location of the root of public_html
+	private $db;		   // the database object
+	private $sanitizer;    // sanitizer object that holds sanitization functionality
 
+	/**
+	 * Constructor for the AccountCreator object.
+	 * Initializes the database and the root path.
+	 * 
+	 * @param string $path_to_root [path to the public_html root]
+	 */
 	public function __construct($path_to_root){
 		$this->path_to_root = $path_to_root;
 
@@ -15,6 +32,19 @@ class AccountCreator{
 		$this->sanitizer = new Sanitizer();
 	}
 
+	/**
+	 * Creates a new account in the database.
+	 * 
+	 * @param  string $_fname       first name
+	 * @param  string $_lname       last name
+	 * @param  int    $_role        role id
+	 * @param  string $_email       email of the user
+	 * @param  string $_pass        password
+	 * @param  string $_confirmpass confirmed password
+	 * @param  string $_contactNum  contact number
+	 * @param  string $_congName    congregation name
+	 * @return string               message about account
+	 */
 	function createNewAccount($_fname, $_lname, $_role, $_email, $_pass, $_confirmpass, $_contactNum="",$_congName=""){
 		$sanitizerResult = $this->sanitizeValidateData($_fname, $_lname, $_role, $_email, $_pass, $_confirmpass);
 
@@ -29,11 +59,11 @@ class AccountCreator{
 					// Create new account
 					$uid = $this->db->insertNewUser($_fname, $_lname, $_role, $_email, $_pass);
 					switch ($_role) {
-						case 4:
+						case 4: // congregation account
 							$this->db->insertNewCongregation($uid,$_congName);
 							break;
 						
-						case 5:
+						case 5: // bus driver account
 							$this->db->insertNewBusDriver($uid,$_contactNum);
 							break;
 
@@ -51,6 +81,13 @@ class AccountCreator{
 		}			
 	}
 
+	/**
+	 * Checks to make sure the email provided is not
+	 * already in use.
+	 * 		
+	 * @param  string $_email email provided			
+	 * @return boolean        true if unique email
+	 */
 	function checkUniqueEmail($_email){
 		$users = $this->db->getUserByEmail($_email);
 
@@ -61,6 +98,18 @@ class AccountCreator{
 		return true;
 	}
 
+	/**
+	 * Sanitizes the data intended to be used in the
+	 * creation of an account.
+	 * 
+	 * @param  string $fname       first name
+	 * @param  string $lname       last name
+	 * @param  int $role           role id
+	 * @param  string $email       email
+	 * @param  string $pass        password
+	 * @param  string $confirmpass confirmed password
+	 * @return string              "Clear" if everything is good
+	 */
 	function sanitizeValidateData($fname, $lname, $role, $email, $pass, $confirmpass){
 		if(!$this->sanitizer->sanitizeEmail($email)){
 			return "Invalid characters used in email.";
@@ -93,6 +142,5 @@ class AccountCreator{
 		return "Clear";
 	}
 }
-
 
 ?>
